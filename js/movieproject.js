35bc1d77696053907dc2916793e2926b95b12d34
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2022-12-05 11:39:18 -0600

    working on card population

--- js/movieproject.js
+++ js/movieproject.js
@@ -2,10 +2,16 @@
 
 $(document).ready(function () {
 
+    console.log("ran")
 
     fetch('https://green-peppermint-quarter.glitch.me/movies').then((response) => {
+        console.log("ran");
         response.json().then((data) => {
             console.log(data);
+            data.forEach((movie) => {
+                console.log(movie);
+                $('#movieContainer').append(`<div class="d-flex flex-column" id="movie"><img src="" alt=""><h5><span id="movieTitle"></span></h5><p><span id="movieDesc"></span></p></div>`)
+            })
         })
     })
 })
