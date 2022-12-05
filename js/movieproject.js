c979229b1efee57f66c1b945eb19f713703440fc
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2022-12-05 16:52:43 -0600

    working on functions for adding and deleting movies

--- js/movieproject.js
+++ js/movieproject.js
@@ -1,7 +1,13 @@
 // 'use strict';
 
 $(document).ready(function () {
+
     setTimeout(function () {
+        loadMovies();
+    }, 2000);
+
+    function loadMovies() {
+        $('#movieContainer').html('');
         fetch('https://green-peppermint-quarter.glitch.me/movies').then((response) => {
             $('.loader').addClass('d-none');
             response.json().then((data) => {
@@ -12,18 +18,17 @@ $(document).ready(function () {
                     <div class="card" style="width: 30rem;" >
                         <img src="${movie.Poster}" class="card-img-top" alt="poster">
                             <div class="card-body">
-                                <h5 class="card-title">${movie.Title}</h5>
+                                <h5 class="card-title">${movie.Title} (${movie.Year})</h5>
                                 <p class="card-text">${movie.Plot}</p>
                                 <p class="card-text">Ratings: ${movie.Ratings[0].Value}</p>
                                 <p class="card-text"> Genre: ${movie.Genre}</p>
-
                             </div>
                     </div>
                 </div>`)
                 })
             })
         })
-    }, 2000);
+    }
 
 
     const apiKey = 'd684bbda';
@@ -52,10 +57,6 @@ $(document).ready(function () {
         fetch('https://green-peppermint-quarter.glitch.me/movies/' + id).then((response) => {
             response.json().then((data) => {
                 console.log(data);
-                // let filteredMovie = data.filter(function(movie){
-                //     return movie.id === 1;
-                // })
-                // console.log(filteredMovie);
                 const url = 'https://green-peppermint-quarter.glitch.me/movies/' + id;
                 const options = {
                     method: 'DELETE',
@@ -70,20 +71,58 @@ $(document).ready(function () {
             })
         })
     }
-})
-const exampleModal = document.getElementById('exampleModal')
-exampleModal.addEventListener('show.bs.modal', event => {
-    // Button that triggered the modal
-    const button = event.relatedTarget
-    // Extract info from data-bs-* attributes
-    const recipient = button.getAttribute('data-bs-whatever')
-    // If necessary, you could initiate an AJAX request here
-    // and then do the updating in a callback.
-    //
-    // Update the modal's content.
-    const modalTitle = exampleModal.querySelector('.modal-title')
-    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+    // deleteMovie(9);
+    // deleteMovie(10);
 
-    modalTitle.textContent = `New Movie`
-    modalBodyInput.value = recipient
+    function addMovie() {
+        let newMovie = {
+            Title: $('#movie-title').val(),
+            Year: $('#year-released').val(),
+            Ratings: [
+                {
+                    Value: $('#movie-rating').val()
+                }
+            ],
+            Genre: $('#movie-genre').val(),
+            Plot: $('#movie-description').val(),
+            Poster: 'https://placeholder.pics/svg/1000x1000'
+        }
+        const url = 'https://green-peppermint-quarter.glitch.me/movies';
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newMovie)
+        };
+        fetch(url, options)
+            .then(response => response.json()).then(data => console.log(data)) /* review was created successfully */
+            .catch(error => console.error(error)); /* handle errors */
+        setTimeout(function (){
+            loadMovies();
+        }, 1000)
+    }
+
+    $('#createNewMovie').click(function (e) {
+        e.preventDefault();
+        addMovie();
+    })
+
+    //modal functions
+    const exampleModal = document.getElementById('exampleModal')
+    exampleModal.addEventListener('show.bs.modal', event => {
+        // Button that triggered the modal
+        const button = event.relatedTarget
+        // Extract info from data-bs-* attributes
+        const recipient = button.getAttribute('data-bs-whatever')
+        // If necessary, you could initiate an AJAX request here
+        // and then do the updating in a callback.
+        //
+        // Update the modal's content.
+        const modalTitle = exampleModal.querySelector('.modal-title')
+        const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+        modalTitle.textContent = `New Movie`
+        modalBodyInput.value = recipient
+    })
 })
