be76a958444102d603cfbecc8500232a930486b0
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2022-12-06 14:31:56 -0600

    finished delete function

--- js/movieproject.js
+++ js/movieproject.js
@@ -40,14 +40,14 @@ $(document).ready(() => {
         filteredMovies.forEach((movie) => {
             $('#movieContainer').append(
                 `<div class=" d-flex col-2" id="movie">
-                    <div class="card" style="width: 30rem;" >
+                    <div class="card">
                         <img src="${movie.Poster}" class="card-img-top" alt="poster">
                             <div class="card-body">
                                 <h5 class="card-title">${movie.Title} (${movie.Year}) </h5>
                                 <p class="card-text">${movie.Plot}</p>
                                 <p class="card-text">Ratings: ${movie.Ratings[0].Value}</p>
                                 <p class="card-text"> Genre: ${movie.Genre}</p>
-                                <div class="card-icons" style="width: 60px">
+                                <div class="card-icons">
                                 <button type="button" class="btn btn-warning editButton"> 
                                 <div class="modal-dialog modal-lg">
                                 Edit <i class="bi bi-pencil-square "></i>
@@ -58,14 +58,23 @@ $(document).ready(() => {
 </div>
 </button>
 <button type="submit" class="btn btn-danger deleteButton" value="${movie.id}">Delete 
-<i class="bi bi-trash3"></i>
-<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
-</svg>
+<!--<i class="bi bi-trash3"></i>-->
+<!--<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">-->
+<!--  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>-->
+<!--</svg>-->
 </button>
 </div>
 </div>
+</div>
 </div>`)
+        })
+        $(`.deleteButton`).click(function (e) {
+            e.preventDefault();
+            console.log('ran delete function')
+            deleteMovie($('.deleteButton').val());
+            setTimeout(() => {
+                loadMovies();
+            }, 1000);
         })
     }
 
@@ -119,14 +128,7 @@ $(document).ready(() => {
         }, 1000)
     })
 
-    $(`#deleteButton`).click(function (e) {
-        e.preventDefault();
-        console.log('ran delete function')
-        deleteMovie($('.deleteButton').val());
-        setTimeout(() => {
-            loadMovies();
-        }, 1000);
-    })
+
 
     $('.editButton').click(function (e) {
         console.log("edit button");
